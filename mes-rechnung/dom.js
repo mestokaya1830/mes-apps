c55abe755d4930cf94b795c90886a3e95860bc7b
mesto <123832169+mestokaya1830@users.noreply.github.com>
2025-07-11 21:32:26 +0200

    Update dom.js

--- mes-rechnung/dom.js
+++ mes-rechnung/dom.js
@@ -1,11 +1,3 @@
- document.getElementById('invoiceForm').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const address = document.getElementById('address').value;
-    const amount = parseFloat(document.getElementById('amount').value);
-    const vat = parseFloat(document.getElementById('vat').value);
-
-    const total = (amount * (1 + vat / 100)).toFixed(2);
-
-    alert(`Rechnung für ${name} (${address}) wird erstellt.\nGesamtbetrag (inkl. MwSt): ${total} €`);
-  });
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello Electron')
+})
