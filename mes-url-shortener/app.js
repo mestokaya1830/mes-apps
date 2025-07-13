ec1fd6de646a0321310917106ab51b1f82d9a02f
mestokaya1830 <mesto1830@outllok.com>
2025-07-13 18:14:18 +0200

    mmm

--- mes-url-shortener/app.js
+++ mes-url-shortener/app.js
@@ -5,7 +5,9 @@
   import helmet from 'helmet'
   import Shortener from './model/shortenerSC.js'
   import path from 'path'
+  import { fileURLToPath } from 'url';
   
+  const __dirname = path.dirname(fileURLToPath(import.meta.url))
   app.use(helmet())
   app.use(express.json())
   app.use(express.static('dist'));
@@ -36,7 +38,7 @@
   if(process.env.NODE_ENV == 'production'){
     app.use(express.static('dist'))
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve('dist/index.html'))
+      res.sendFile(path.resolve(__dirname, 'dist/index.html'))
     })
   }
   app.use((req, res) => {
@@ -46,7 +48,6 @@
   app.use((error, req, res, next) => {
     console.log(error)
     res.json({error: error})
-    next()
   })
   app.listen(process.env.PORT, () => {
     console.log('Server is running...')
