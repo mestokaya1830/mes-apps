a37a278a5204f4fc0cfb74b2793451f3da05bc1a
mestokaya1830 <mesto1830@outllok.com>
2025-09-26 18:56:31 +0200

    mkm

--- invoices-vue/src/renderer/src/views/auth/Settings.vue
+++ invoices-vue/src/renderer/src/views/auth/Settings.vue
@@ -1,7 +1,30 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <form>
+      <div>
+        <label for="bank">Bank Name</label>
+        <input id="bank" v-model="settings.bank_name" type="text" />
+      </div>
+      <div>
+        <label for="iban">Iban</label>
+        <input id="iban" v-model="settings.iban" type="text" />
+      </div>
+      <div>
+        <label for="bank_account_holder">Bank Account Holder</label>
+        <input id="bank_account_holder" v-model="settings.bank_account_holder" type="text" />
+      </div>
+      <div>
+        <label for="bic">Bic</label>
+        <input id="bic" v-model="settings.bic" type="text" />
+      </div>
+      <div>
+        <label for="language">Language</label>
+        <input id="language" v-model="settings.language" type="text" />
+      </div>
+    </form>
     {{ settings }}
+    <button @click="saveSettings()">Save</button>
   </div>
 </template>
 
@@ -15,11 +38,16 @@ export default {
   },
   mounted() {
     this.settings = {
-      bank: this.$store.state.auth.bank,
+      bank_name: this.$store.state.auth.bank_name,
       iban: this.$store.state.auth.iban,
       bank_account_holder: this.$store.state.auth.bank_account_holder,
       bic: this.$store.state.auth.bic,
       language: this.$store.state.auth.language
+    }
+  },
+  methods: {
+    async saveSettings() {
+      console.log(this.settings)
     }
   }
 }
