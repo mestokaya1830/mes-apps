3ee0a4b67370bd84820f7b2157e24881a6d66248
mestokaya1830 <mesto1830@outllok.com>
2025-11-28 01:17:38 +0100

    vfvfv

--- invoices-vue/src/main/index.js
+++ invoices-vue/src/main/index.js
@@ -78,7 +78,21 @@ ipcMain.handle('control-window', (event, data) => {
   }
 })
 
-//register and login
+//register
+ipcMain.handle('check-register', async () => {
+  try {
+    const row = db.prepare('SELECT * FROM users').all()
+    if (row.length > 0) {
+      return { success: true }
+    } else {
+      return { success: false }
+    }
+  } catch (err) {
+    console.error('DB error:', err.message)
+    return { success: false, message: err.message }
+  }
+})
+
 ipcMain.handle('register', async (event, image, data) => {
   try {
     if (data) {
@@ -154,6 +168,132 @@ ipcMain.handle('register', async (event, image, data) => {
     }
   } catch (error) {
     console.log(error)
+  }
+})
+
+
+
+//user and login
+ipcMain.handle('login', async (event, data) => {
+  const { email, password } = data
+  try {
+    const row = db
+      .prepare('SELECT * FROM users WHERE email = ? AND password = ?')
+      .get(email, password)
+
+    if (row) {
+      return { success: true, user: row }
+    } else {
+      return { success: false, message: 'Invalid email or password' }
+    }
+  } catch (err) {
+    console.error('DB error:', err.message)
+    return { success: false, message: err.message }
+  }
+})
+
+ipcMain.handle('reset-password', async (event, data) => {
+  try {
+    db.prepare("DELETE FROM tokens WHERE replace(expires_at, 'T', ' ') < datetime('now')").run()
+    const { token, password } = data
+    const tokenRow = db.prepare('SELECT * FROM tokens WHERE token = ?').get(token)
+    if (!tokenRow) {
+      return { success: false, message: 'Invalid token' }
+    } else {
+      const userRow = db.prepare('SELECT * FROM users WHERE id = ?').get(tokenRow.user_id)
+      if (!userRow) {
+        return { success: false, message: 'User not found' }
+      } else {
+        db.prepare('UPDATE users SET password = ? WHERE id = ?').run(password, userRow.id)
+        db.prepare('DELETE FROM tokens WHERE token = ?').run(token)
+        return { success: true, message: 'Password reset successfully' }
+      }
+    }
+  } catch (err) {
+    console.error('DB error:', err.message)
+    return { success: false, message: err.message }
+  }
+})
+
+ipcMain.handle('email-verfication', async (event, data) => {
+  const { email } = data
+  const expiresAt = new Date(Date.now() + 60000 * 1)
+  const token = Math.random().toString(36).substring(2, 50)
+  try {
+    const user = db.prepare('SELECT * FROM users WHERE email = ?').get(email)
+    if (user) {
+      db.prepare('INSERT INTO tokens (token, user_id, expires_at) VALUES (?,?,?)').run(
+        token,
+        user.id,
+        expiresAt.toISOString()
+      )
+      return { success: true, message: 'Email sent and token insert' }
+    } else {
+      return { success: false, message: 'No user found with that email' }
+    }
+  } catch (err) {
+    console.error('DB error:', err.message)
+    return { success: false, message: 'DB insert failed' }
+  }
+
+  // const { email } = data
+  // const token = Math.random().toString(36).substring(2, 50)
+  // const expiresAt = new Date(Date.now() + 60000 * 15) // 15 minutes
+  // //email host settings
+  // const transporter = nodeMailer.createTransport({
+  //   host: 'smtp.gmail.com',
+  //   port: 465,
+  //   auth: {
+  //     user: 'mesto1830@gmail.com',
+  //     pass: 'taao aisb gkkc evwx'
+  //   }
+  // })
+  // //email options
+  // const mailoption = {
+  //   from: 'mesto1830@gmail.com',
+  //   to: email,
+  //   subject: 'Password Reset Request',
+  //   text: 'To reset password please click link below...',
+  //   html: `
+  //   <h2>To reset password please use this token'</h2>
+  //   <h1>${token}</h1>
+  //   `
+  // }
+  // transporter.sendMail(mailoption, (err, info) => {
+  //   if (err) {
+  //     console.error('Email error:', err.message)
+  //     return { success: false, message: err.message }
+  //   } else {
+  //     console.log('Email sent:', info.response, token)
+  //     try {
+  //       const row = db
+  //         .prepare('INSERT INTO tokens (token, user_id, expires_at) VALUES (?,?,?)')
+  //         .run(token, 1, expiresAt)
+  //       if (!row) {
+  //         console.warn('No user found with that email')
+  //         return { success: false, message: 'No user found with that email' }
+  //       } else {
+  //         return { success: true, message: 'Email sent and token updated' }
+  //       }
+  //     } catch {
+  //       console.error('DB error:', err.message)
+  //       return { success: false, message: err.message }
+  //     }
+  //   }
+  // })
+})
+
+ipcMain.handle('get-user', async () => {
+  try {
+    const row = db.prepare('SELECT * FROM users WHERE id = 1').get()
+    if (row && row.logo) {
+      // Buffer → Base64 string
+      row.logo = row.logo.toString('base64')
+    }
+    return { success: true, user: row }
+  } catch (err) {
+    console.error('DB error:', err.message)
+    return { success: false, message: err.message }
   }
 })
 
@@ -239,144 +379,6 @@ ipcMain.handle('update-user', async (event, image, data) => {
   } catch (error) {
     console.error('updateProfile error:', error)
     return { success: false, message: error.message }
-  }
-})
-
-ipcMain.handle('check-register', async () => {
-  try {
-    const row = db.prepare('SELECT * FROM users').all()
-    if (row.length > 0) {
-      return { success: true }
-    } else {
-      return { success: false }
-    }
-  } catch (err) {
-    console.error('DB error:', err.message)
-    return { success: false, message: err.message }
-  }
-})
-
-ipcMain.handle('email-verfication', async (event, data) => {
-  const { email } = data
-  const expiresAt = new Date(Date.now() + 60000 * 1)
-  const token = Math.random().toString(36).substring(2, 50)
-  try {
-    const user = db.prepare('SELECT * FROM users WHERE email = ?').get(email)
-    if (user) {
-      db.prepare('INSERT INTO tokens (token, user_id, expires_at) VALUES (?,?,?)').run(
-        token,
-        user.id,
-        expiresAt.toISOString()
-      )
-      return { success: true, message: 'Email sent and token insert' }
-    } else {
-      return { success: false, message: 'No user found with that email' }
-    }
-  } catch (err) {
-    console.error('DB error:', err.message)
-    return { success: false, message: 'DB insert failed' }
-  }
-
-  // const { email } = data
-  // const token = Math.random().toString(36).substring(2, 50)
-  // const expiresAt = new Date(Date.now() + 60000 * 15) // 15 minutes
-  // //email host settings
-  // const transporter = nodeMailer.createTransport({
-  //   host: 'smtp.gmail.com',
-  //   port: 465,
-  //   auth: {
-  //     user: 'mesto1830@gmail.com',
-  //     pass: 'taao aisb gkkc evwx'
-  //   }
-  // })
-  // //email options
-  // const mailoption = {
-  //   from: 'mesto1830@gmail.com',
-  //   to: email,
-  //   subject: 'Password Reset Request',
-  //   text: 'To reset password please click link below...',
-  //   html: `
-  //   <h2>To reset password please use this token'</h2>
-  //   <h1>${token}</h1>
-  //   `
-  // }
-  // transporter.sendMail(mailoption, (err, info) => {
-  //   if (err) {
-  //     console.error('Email error:', err.message)
-  //     return { success: false, message: err.message }
-  //   } else {
-  //     console.log('Email sent:', info.response, token)
-  //     try {
-  //       const row = db
-  //         .prepare('INSERT INTO tokens (token, user_id, expires_at) VALUES (?,?,?)')
-  //         .run(token, 1, expiresAt)
-  //       if (!row) {
-  //         console.warn('No user found with that email')
-  //         return { success: false, message: 'No user found with that email' }
-  //       } else {
-  //         return { success: true, message: 'Email sent and token updated' }
-  //       }
-  //     } catch {
-  //       console.error('DB error:', err.message)
-  //       return { success: false, message: err.message }
-  //     }
-  //   }
-  // })
-})
-
-//user
-ipcMain.handle('login', async (event, data) => {
-  const { email, password } = data
-  try {
-    const row = db
-      .prepare('SELECT * FROM users WHERE email = ? AND password = ?')
-      .get(email, password)
-
-    if (row) {
-      return { success: true, user: row }
-    } else {
-      return { success: false, message: 'Invalid email or password' }
-    }
-  } catch (err) {
-    console.error('DB error:', err.message)
-    return { success: false, message: err.message }
-  }
-})
-
-ipcMain.handle('reset-password', async (event, data) => {
-  try {
-    db.prepare("DELETE FROM tokens WHERE replace(expires_at, 'T', ' ') < datetime('now')").run()
-    const { token, password } = data
-    const tokenRow = db.prepare('SELECT * FROM tokens WHERE token = ?').get(token)
-    if (!tokenRow) {
-      return { success: false, message: 'Invalid token' }
-    } else {
-      const userRow = db.prepare('SELECT * FROM users WHERE id = ?').get(tokenRow.user_id)
-      if (!userRow) {
-        return { success: false, message: 'User not found' }
-      } else {
-        db.prepare('UPDATE users SET password = ? WHERE id = ?').run(password, userRow.id)
-        db.prepare('DELETE FROM tokens WHERE token = ?').run(token)
-        return { success: true, message: 'Password reset successfully' }
-      }
-    }
-  } catch (err) {
-    console.error('DB error:', err.message)
-    return { success: false, message: err.message }
-  }
-})
-
-ipcMain.handle('get-user', async () => {
-  try {
-    const row = db.prepare('SELECT * FROM users WHERE id = 1').get()
-    if (row && row.logo) {
-      // Buffer → Base64 string
-      row.logo = row.logo.toString('base64')
-    }
-    return { success: true, user: row }
-  } catch (err) {
-    console.error('DB error:', err.message)
-    return { success: false, message: err.message }
   }
 })
 
--- invoices-vue/src/preload/preload.js
+++ invoices-vue/src/preload/preload.js
@@ -1,24 +1,31 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
+  //window control
   controlWindow: (data) => ipcRenderer.invoke('control-window', data),
 
-  checkRegister: async (data) => ipcRenderer.invoke('check-register', data),
+  //register
   register: async (image, data) => ipcRenderer.invoke('register', image, data),
+  checkRegister: async (data) => ipcRenderer.invoke('check-register', data),
+
+  //login and user
   login: async (data) => ipcRenderer.invoke('login', data),
-  emailVerfication: async (data) => ipcRenderer.invoke('email-verfication', data),
   resetPassword: async (data) => ipcRenderer.invoke('reset-password', data),
+  emailVerfication: async (data) => ipcRenderer.invoke('email-verfication', data),
   getUser: async (data) => ipcRenderer.invoke('get-user', data),
   updateUser: async (image, data) => ipcRenderer.invoke('update-user', image, data),
 
+  //dashboard
   getDashboard: async (data) => ipcRenderer.invoke('get-dashboard', data),
 
+  //customer
   addCustomer: async (data) => ipcRenderer.invoke('add-customer', data),
   getCustomers: async (data) => ipcRenderer.invoke('get-customers', data),
   getCustomerById: async (data, id) => ipcRenderer.invoke('get-customer-by-id', data, id),
   updateCustomerById: async (data, id) => ipcRenderer.invoke('update-customer-by-id', id, data),
   deleteCustomerById: async (data) => ipcRenderer.invoke('delete-customer-by-id', data),
   
+  //invoice
   saveDocument: async (id, tableName, data) =>
     ipcRenderer.invoke('save-document', id, tableName, data),
   getDocument: async (data, tableName) => ipcRenderer.invoke('get-document', data, tableName),
